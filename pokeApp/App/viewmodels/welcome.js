dfb77bf6ca404934cb5b0be4929531b1e66ab6be
Andreia <andreia.portela.2001@gmail.com>
2019-12-18 14:30:56 +0000

    Fase 3

--- pokeApp/App/viewmodels/welcome.js
+++ pokeApp/App/viewmodels/welcome.js
@@ -30,14 +30,10 @@ function add_hide() {
 };
 
 define(['durandal/app'], function (app) {
-    var i = setInterval(function () {
-
-        clearInterval(i);
-        document.getElementById("loading").style.display = "none";
-        document.getElementById("conteudo").style.display = "inline";
-    }, 4000);
+    
 
     var page = function () {
+        
         var self = this;
         self.displayName = 'Pokémon Life';
         self.description = 'Pokémon are the creatures that inhabit the world of the Pokémon games. They can be caught using Pokéballs and trained by battling with other Pokémon. Each Pokémon belongs to a specific species but may take on a variant which makes it differ from other Pokémon of the same species, such as base stats, available abilities and typings.<br/>Pokémon are raised and commanded by their owners (called “Trainers”). During their adventures, Pokémon grow, level up and become more experienced and even, on occasion, evolve into stronger Pokémon.';
@@ -49,6 +45,13 @@ define(['durandal/app'], function (app) {
             'And much more!',
             
         ];
+        var i = setInterval(function () {
+
+        clearInterval(i);
+        document.getElementById("loading").style.display = "none";
+        document.getElementById("conteudo").style.display = "inline";
+        }, 4000);
+        return i;
     };
 
 
